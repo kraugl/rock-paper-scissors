e115ab8ea15e7caa5d786cd7f89ec9226fd0115a
edou <edouardjade01@gmail.com>
2022-10-17 20:57:47 +0800

    Add playRound function

--- index.js
+++ index.js
@@ -5,3 +5,21 @@ const computerChoices = ['rock','paper','scissors'];
 function getComputerChoices(){
     return computerChoices[Math.floor(Math.random() * computerChoices.length)];
 };
+
+function playRound(playerSelection, computerSelection){
+    let player = playerSelection.toLowercase();
+    let computer = computerSelection;
+    let youWin = 'You win!!!' + player + ' beats ' + computerSelection;
+    let youLose = 'You lose!!!' + player + ' beats ' + computerSelection;
+    let draw = 'Draw!!! play again!!!'
+
+    if((player === 'rock' && computer === 'scissors') || (player === 'scissors' && computer === 'paper') || (player === 'paper' && computer === 'rock')){
+        return youWin;
+    } else if((computer === 'rock' && player === 'scissors') || (computer === 'scissors' && player === 'paper') || (computer === 'paper' && player === 'rock')){
+        return youLose;
+    } else if(player === computer){
+        return draw;
+    } else {
+        return 'Choose between rock, paper or scissors!!!'
+    };
+};
