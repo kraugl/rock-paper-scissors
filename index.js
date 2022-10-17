dc20ac96f9056fbf7b3a7a812e0ae617082156a1
edou <edouardjade01@gmail.com>
2022-10-17 20:43:31 +0800

    fix getComputerChoices

--- index.js
+++ index.js
@@ -1,3 +1,7 @@
 const computerChoices = ['rock','paper','scissors'];
 
-const getComputerChoices = computerChoices[Math.floor(Math.random() * (computerChoices.length - 1))]
+//let getComputerChoices = computerChoices[Math.floor(Math.random() * (computerChoices.length - 1))];
+
+function getComputerChoices(){
+    return computerChoices[Math.floor(Math.random() * computerChoices.length)];
+};
