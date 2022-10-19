52a4b6a5ff1ce865d30386049261f37b235a7737
edou <edouardjade01@gmail.com>
2022-10-19 13:09:05 +0800

    Add game function

--- index.js
+++ index.js
@@ -1,29 +1,60 @@
-let playerSelection;
-let computerSelection;
+const computerChoices = ['rock','paper','scissors'];
+
+let playerSelection = prompt('Rock, Paper, or Scissors', '').toLowerCase();
+let computerSelection = getComputerChoice();
 let playerScore = 0;
 let computerScore = 0;
 
-const computerChoices = ['rock','paper','scissors'];
 
 function getComputerChoice(){
     return computerChoices[Math.floor(Math.random() * computerChoices.length)];
 };
 
 function playRound(playerSelection, computerSelection){
-    let player = playerSelection.toLowerCase();
-    let computer = computerSelection;
-    let youWin = 'You win!!! ' + player + ' beats ' + computer;
-    let youLose = 'You lose!!! ' + computer + ' beats ' + player;
+
+    let youWin = 'You win!!! ' + playerSelection + ' beats ' + computerSelection;
+    let youLose = 'You lose!!! ' + computerSelection + ' beats ' + playerSelection;
     let draw = 'Draw!!! play again!!!'
 
-    if((player === 'rock' && computer === 'scissors') || (player === 'scissors' && computer === 'paper') || (player === 'paper' && computer === 'rock')){
+    if((playerSelection === 'rock' && computerSelection === 'scissors') || 
+       (playerSelection === 'scissors' && computerSelection === 'paper') || 
+       (playerSelection === 'paper' && computerSelection === 'rock')){
         return youWin;
-    } else if((computer === 'rock' && player === 'scissors') || (computer === 'scissors' && player === 'paper') || (computer === 'paper' && player === 'rock')){
+    } else if((computerSelection === 'rock' && playerSelection === 'scissors') || 
+              (computerSelection === 'scissors' && playerSelection === 'paper') || 
+              (computerSelection === 'paper' && playerSelection === 'rock')){
         return youLose;
-    } else if(player === computer){
+    } else if(playerSelection === computerSelection){
         return draw;
     } else {
         return 'Choose between rock, paper or scissors!!!'
     };
 };
 
+function game(){
+    for(i = 0; i < 5; i++){
+        computerSelection = getComputerChoice();
+        let youWin = 'You win!!! ' + playerSelection + ' beats ' + computerSelection;
+        let youLose = 'You lose!!! ' + computerSelection + ' beats ' + playerSelection;
+        let draw = 'Draw!!! play again!!!'
+        let result = playRound(playerSelection, computerSelection);
+
+        if(result === youWin){
+            console.log(result + `!!! Player: ${++playerScore} | Computer: ${computerScore}`);
+        }else if(result === youLose){
+            console.log(result + `!!! Player: ${playerScore} | Computer: ${++computerScore}`)
+        }else if(result === draw){
+            console.log(result + `!!! Player: ${playerScore} | Computer: ${computerScore}`)
+        }else{
+            console.log('error');
+        }
+    }
+
+    if(playerScore > computerScore){
+        return `You win ${playerScore}:${computerScore}`;
+    }else if(playerScore < computerScore){
+        return `You lose ${playerScore}:${computerScore}`;
+    }else return 'draw';
+}
+
+console.log(game())
